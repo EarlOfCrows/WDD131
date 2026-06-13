5d3074fbd330a0bb170a7035749a36359e12dfb6
Eric Flamm <ericwflamm@gmail.com>
2026-06-13 15:47:33 -0600

    char-card

---
+++ ponder/char-card/char-card.js
@@ -0,0 +1,34 @@
+let classElement = document.querySelector('.stats p.class')
+let levelElement = document.querySelector('.stats p.level')
+let healthElement = document.querySelector('.stats p.health')
+
+const card_stats = {
+  class: "Swamp Beast Diplomat",
+  level: 5,
+  health: 100
+};
+
+const attackedButton = document.querySelector(".attacked")
+const levelUpButton = document.querySelector(".level-up")
+
+attackedButton.addEventListener("click", () => {
+    card_stats.health -= 20;
+    updateCardStats();
+    if (card_stats.health <= 0) {
+        alert("Character Died");
+        customAlert.classList.remove('hidden')
+    }
+});
+
+levelUpButton.addEventListener("click", () => {
+    card_stats.level ++
+    updateCardStats();
+});
+
+function updateCardStats() {
+  classElement.textContent = `Class: ${card_stats.class}`;
+  levelElement.textContent = `Level: ${card_stats.level}`;
+  healthElement.textContent = `Health: ${card_stats.health}`;
+}
+
+updateCardStats();
